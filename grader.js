03029423b80ea6d54cc7d7752e928ae59b69a26d
Hery <hery1204@gmail.com>
2013-07-17 14:58:59 +0000

    hw3 completed

--- grader.js
+++ grader.js
@@ -23,8 +23,10 @@ references:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = 'index.html';
 var CHECKSFILE_DEFAULT = 'checks.json';
+var URL_DEFAULT = 'http://www.google.com';
 
 var assertFileExists = function(infile) {
  var instr = infile.toString();
@@ -54,6 +56,8 @@ var checkHtmlFile = function(htmlfile, checksfile) {
  return out;
 };
 
+var checkUrlPath = function(urlpath, checksfile) {}
+
 var clone = function(fn) {
  //Workaround for commander.js issue
  //http://stackoverflow.com/a/6772648
@@ -64,11 +68,40 @@ if(require.main == module) {
  program
   .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
   .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+  .option('-u, --url <url_path>', 'URL Path to checked')
   .parse(process.argv);
- 
- var checkJson = checkHtmlFile(program.file, program.checks);
- var outJson = JSON.stringify(checkJson, null, 4);
- console.log(outJson);
+
+ var checkJson; 
+ if(program.url == null || program.url == '')
+ {
+    console.log("checking File: %s", program.file);
+    checkJson = checkHtmlFile(program.file, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+ }
+ else if(program.url != null && program.url != '')
+ { 
+   console.log("checking URL: %s", program.url);
+   //checkJson = checkUrlPath(program.url, program.checks);
+   restler.get(program.url).on('complete', function(result){
+     if(result instanceof Error){
+      console.log("Problem getting the specified URL: %s", result);
+      process.exit(1);
+     }else{
+      $=cheerio.load(result);
+      var checks = loadChecks(program.checks).sort();
+      var out = {};
+      for(var ii in checks) {
+       var present = $(checks[ii]).length > 0;
+       out[checks[ii]] = present;
+      }
+      console.log(JSON.stringify(out, null, 4));
+     }
+   });
+ }else
+ {
+   console.log("no file nor url parameter found. check --help for usage");
+ }
 } else {
  exports.checkHtmlFile = checkHtmlFile;
 }
